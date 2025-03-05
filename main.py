64339406e73c73e4f25198ddcd8f82321274dc6c
Daniel George <danielgeorgeub@gmail.com>
2025-03-04 21:00:23 -0500

    feat se crea opción1 para poblar diccionario y lista de productos

--- main.py
+++ main.py
@@ -1,11 +1,41 @@
 nombreVendedor=None
 productos=[]
-producto=[]
+producto={}
 
 opcion=100
 
 print("Mercado")
+print("**********")
 print("1. Crear lista de mercado")
+print("2. Ver lista de mercado")
+print("3. Editar producto de la lista")
+print("4. Retirar producto de la lista")
 print("Presiona 5 para salir")
 while opcion != 5:
     opcion=int(input("Digita una opción: "))
+    if opcion == 1:
+        print("Bienvenido a la creación de tu lista de mercado")
+
+        #Creando claves y valores de un diccionario
+        producto["id"]=5
+        producto["nombre"]=input("Digita el nombre del producto: ")
+        producto["precio"]=int(input("Digita el precio del producto: "))
+        producto["cantidad"]=int(input("Cuantos elementos de este producto vas a llevar: "))
+        producto["presentacion"]=input("¿Cual presentación llevaras? ")
+
+        #mostrando mi diccionario
+        #print(producto)
+
+        #poblando una lista (agregando elementos a una lista)
+        productos.append(producto)
+        print(productos)
+
+
+    elif opcion == 2:
+        print("Estoy en la 2")
+    elif opcion == 3:
+        print("Estoy en la 3")
+    elif opcion == 4:
+        print("Estoy en la 4")
+    else:
+        print("Opción no válida")
